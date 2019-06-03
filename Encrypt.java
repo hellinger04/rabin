a7d0a81843092de91f673d92d89fd3a128fff1c1
Andrew Hellinger <hellinger@jhu.edu>
2019-06-03 12:51:53 -0400

    can calculate GCD of two positive integers

---
+++ Encrypt.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Encrypt {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        //prompt for user input
+        System.out.print("Enter the two positive integers to calculate GCD for: ");
+        int int1 = kb.nextInt();
+        int int2 = kb.nextInt();
+
+        //we cannot calculate the GCD with negative or zero integers
+        if (int1 < 1 || int2 < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        //if all good, calculate GCD and print results to screen
+        int gcd = euclid(int1, int2);
+        System.out.println("The GCD of " + int1 + " and " + int2 + " is " + gcd);
+    }
+
+    private static int euclid(int num1, int num2) {
+        do {
+            //Euclid's algorithm for calculating GCD
+            int remainder = num1 % num2;
+            num1 = num2;
+            num2 = remainder;
+        } while (num2 != 0); //run until we find a common divisor
+
+        //return gcd
+        return num1;
+    }
+}
