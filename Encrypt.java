7944dc5c725cf74602e1d07c862532769650ea36
Andrew Hellinger <hellinger@jhu.edu>
2019-06-04 01:16:33 -0400

    added exception testing and fixed style and formatting

--- Encrypt.java
+++ Encrypt.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Encrypt {
     public static void main(String[] args) {
@@ -13,7 +12,14 @@ public class Encrypt {
 
         //we cannot encrypt with negative or zero integers
         if (pubKey < 1) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("ERROR: Public key cannot be "
+              + "negative");
+        }
+
+        //we cannot currently encrypt strings with length greater than five
+        if (message.length() > 5) {
+            throw new IllegalArgumentException("ERROR: Input message length "
+              + "ccannot be greater than five");
         }
 
         //if all good, convert original message to lowercase and then to
@@ -36,89 +42,90 @@ public class Encrypt {
             char toConvert = message.charAt(i);
 
             switch (toConvert) {
-            case 'a':
-                charValue = 1;
-                break;
-            case 'b':
-                charValue = 2;
-                break;
-            case 'c':
-                charValue = 3;
-                break;
-            case 'd':
-                charValue = 4;
-                break;
-            case 'e':
-                charValue = 5;
-                break;
-            case 'f':
-                charValue = 6;
-                break;
-            case 'g':
-                charValue = 7;
-                break;
-            case 'h':
-                charValue = 8;
-                break;
-            case 'i':
-                charValue = 9;
-                break;
-            case 'j':
-                charValue = 10;
-                break;
-            case 'k':
-                charValue = 11;
-                break;
-            case 'l':
-                charValue = 12;
-                break;
-            case 'm':
-                charValue = 13;
-                break;
-            case 'n':
-                charValue = 14;
-                break;
-            case 'o':
-                charValue = 15;
-                break;
-            case 'p':
-                charValue = 16;
-                break;
-            case 'q':
-                charValue = 17;
-                break;
-            case 'r':
-                charValue = 18;
-                break;
-            case 's':
-                charValue = 19;
-                break;
-            case 't':
-                charValue = 20;
-                break;
-            case 'u':
-                charValue = 21;
-                break;
-            case 'v':
-                charValue = 22;
-                break;
-            case 'w':
-                charValue = 23;
-                break;
-            case 'x':
-                charValue = 24;
-                break;
-            case 'y':
-                charValue = 25;
-                break;
-            case 'z':
-                charValue = 26;
-                break;
-            default:
-                throw new IllegalArgumentException();
+                case 'a':
+                    charValue = 1;
+                    break;
+                case 'b':
+                    charValue = 2;
+                    break;
+                case 'c':
+                    charValue = 3;
+                    break;
+                case 'd':
+                    charValue = 4;
+                    break;
+                case 'e':
+                    charValue = 5;
+                    break;
+                case 'f':
+                    charValue = 6;
+                    break;
+                case 'g':
+                    charValue = 7;
+                    break;
+                case 'h':
+                    charValue = 8;
+                    break;
+                case 'i':
+                    charValue = 9;
+                    break;
+                case 'j':
+                    charValue = 10;
+                    break;
+                case 'k':
+                    charValue = 11;
+                    break;
+                case 'l':
+                    charValue = 12;
+                    break;
+                case 'm':
+                    charValue = 13;
+                    break;
+                case 'n':
+                    charValue = 14;
+                    break;
+                case 'o':
+                    charValue = 15;
+                    break;
+                case 'p':
+                    charValue = 16;
+                    break;
+                case 'q':
+                    charValue = 17;
+                    break;
+                case 'r':
+                    charValue = 18;
+                    break;
+                case 's':
+                    charValue = 19;
+                    break;
+                case 't':
+                    charValue = 20;
+                    break;
+                case 'u':
+                    charValue = 21;
+                    break;
+                case 'v':
+                    charValue = 22;
+                    break;
+                case 'w':
+                    charValue = 23;
+                    break;
+                case 'x':
+                    charValue = 24;
+                    break;
+                case 'y':
+                    charValue = 25;
+                    break;
+                case 'z':
+                    charValue = 26;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
             }
 
-            long toAdd = charValue * ((long) Math.pow(10, (length - i - 1) * 2));
+            long toAdd = charValue * ((long) Math.pow(10,
+                (length - i - 1) * 2));
             result = result + toAdd;
         }
 
